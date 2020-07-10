4a203e6ace3a02fe3774b74ffb0b84b270a6e6c2
Atem Holison <65386938+Holi-Projects@users.noreply.github.com>
2020-07-10 14:29:08 +0800

    Updated QR

--- qrcode.js
+++ qrcode.js
@@ -1,38 +1,31 @@
-const http = require("http")
-const server = http.createServer()
+const app = require("express")()
+const port = process.env.port || 3000
+const bodyparser = require('body-parser') 
 const qr = require("qrcode")
 
-server.on("request", function(req, res){
+app.use(bodyparser.urlencoded({extended:false})) 
+app.use(bodyparser.json()) 
 
-    qr.toString('hope',{type:'svg'}, function (err, 
-        code) {
-         console.log(code) 
-         res.writeHead(200,{'Content-Type': 'text/xml'})
-         //res.end("<img src='" + code + ".png"+"' >")
-         res.end(code)
-        })
+app.get('/', function(req,res){
+  res.sendFile(__dirname + "/index.html");
+})
 
-}).listen(3000)
-
-// const express = require("express")
-// const qr = require("qrcode")
-
-// const EventEmitter = require("events").EventEmitter
-// var app=express();
-// var gen = new EventEmitter()
-
-
-
-// app.get("/", function(req, res){
+app.post('/genQR', function(req,res){
+  console.log(req.body.text)
+  const qrText = req.body.text
+  qr.toString(qrText,{type:'svg'}, function (err, 
+    code) {
+     //console.log(code) 
+     res.writeHead(200,{'Content-Type': 'text/xml'})
+     //res.end("<img src='" + code + ".png"+"' >")
+     res.end(code)
+    })
     
-//      res.sendFile(__dirname + "/index.html")
-// })
 
+})
 
-// gen.on("connection", function(btn){
-//     btn.on('click', function(data){
-//         console.log(data)
-//     })
-// })
+app.listen(port, function(error){
+  if (error) throw error 
+  console.log("Server created Successfully on PORT", port) 
+})
 
-//   var server=app.listen(3000,function() {});
